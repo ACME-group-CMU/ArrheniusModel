a6d62573f92422466aa90488c0af001c077a647d
heyyeahcrow <62277691+heyyeahcrow@users.noreply.github.com>
2024-04-03 22:01:53 -0400

    Add files via upload
    The simulation was successfully run after adding the flow rate factor.
    However,
    1. The function for calculating the flow-rate coefficient still requires some updates
    2. Haven't built the for loop for different flow rates
    3. I haven't added the probability of growth at this time point. It probably won't make too much change to the result
    # I intentionally set the activation energy for beta and kappa to be smaller than others to control the result

--- Energies.jl
+++ Energies.jl
@@ -1,11 +1,10 @@
-G_values = [-5.93, -5.942, -5.97-0.3, -5.948-0.2, -5.937, -5.94] #Amorphous unsure
+G_values = [-5.92, -5.942, -5.97, -5.948, -5.937, -5.94] #Amorphous unsure
 deltaG = [G_values[j] - G_values[i] for j in 1:length(G_values), i in 1:length(G_values)]
-Ea_constants = [0.000 0.001 0.001 0.001 0.001 0.001;  #set the activation energy from amorphous to small
-                0.001 0.000 0.002 0.004 0.005 0.006;
-                0.001 0.002 0.000 0.002 0.002 0.002;
-                0.001 0.004 0.002 0.000 0.004 0.004;
-                0.001 0.005 0.002 0.004 0.000 0.006;
-                0.001 0.006 0.002 0.004 0.006 0.000]
-
+Ea_constants = [0.00 1.00 0.01 0.01 1.00 1.00;  #set the activation energy from amorphous to kappa and beta to be small
+                1.00 0.00 1.00 1.00 1.00 1.00;
+                0.01 1.00 0.00 0.01 1.00 1.00;
+                0.01 1.00 0.01 0.00 1.00 1.00;
+                1.00 1.00 1.00 1.00 0.00 1.00;
+                1.00 1.00 1.00 1.00 1.00 0.00]
 Ea_values = [i == j ? 0 : (deltaG[i, j] > 0 ? deltaG[i, j] + Ea_constants[i, j] : Ea_constants[i, j])
                 for j in 1:length(G_values), i in 1:length(G_values)]
--- newArrheniusEq.jl
+++ newArrheniusEq.jl
@@ -15,27 +15,51 @@
 #include("Energies.jl")
 
 kb = 8.617e-5 #eV/K
-function arrhenius_rate(A, Ea, T)
-    k = A * exp(-Ea / (kb* T))
+"""
+function flow_coefficient(type, effecting_layers, decay_constant)
+    A = 1.0
+    if type == "exponential"
+        return A * exp(-decay_constant * coveringlayers)
+    elseif type == "linear"
+        return A * (1 - decay_constant * coveringlayers)
+    end
+end
+"""
+
+function flow_coefficient(type, effecting_nums, decay_coefficient)
+    # effecting_nums: it approaches zero and stop decreasing as the number of effecting layers increases to a certain point, 
+    #                 so I manually set this to limit the number of effecting layers
+    flow_coefficients = []
+    for i in 1:effecting_nums
+        if type == "exponential"
+            push!(flow_coefficients, exp(-decay_coefficient * i))
+        elseif type == "linear"
+            push!(flow_coefficients, (1 - decay_coefficient * i))
+        end
+    end
+    return flow_coefficients
+end
+
+function arrhenius_rate(Ea, T)
+    k = exp(-Ea / (kb* T))
     return k
 end
 
-function arrhenius_rate_matrix(A, Ea_m, T, dt)
-    k_matrix = [arrhenius_rate(A, Ea_m[i, j], T) for i in 1:size(Ea_m, 1), j in 1:size(Ea_m, 2)]
+function arrhenius_rate_matrix(Ea_m, T)
+    k_matrix = [arrhenius_rate(Ea_m[i, j], T) for i in 1:size(Ea_m, 1), j in 1:size(Ea_m, 2)]
     k = copy(k_matrix)
-    # Adjust the diagonal elements and multiply non-diagonal elements by dt
+    # Adjust the diagonal elements
     for i in 1:size(k_matrix, 1)
         for j in 1:size(k_matrix, 2)
             if i == j
-                k_matrix[i, i] = 1 - dt * sum(k[i, [1:i-1; i+1:end]])
-            else
-                k_matrix[i, j] *= dt
+                k_matrix[i, i] =  -1 * sum(k[i, [1:i-1; i+1:end]])
             end
         end
     end
     return k_matrix
 end
 
+"""
 function arrhenius_transform_with_deposition(existing_layers, new_layer, k_m) #in the time step of dt
     # Transform existing layers
     transformed_layers = existing_layers * k_m
@@ -43,3 +67,4 @@ function arrhenius_transform_with_deposition(existing_layers, new_layer, k_m) #i
 
     return transformed_layers
 end
+"""
