b3c5724cc9eaa2613ba65335905c6eeb7cc0a5e9
Rachel Kurchin <rkurchin@cmu.edu>
2024-05-02 20:08:15 -0400

    basic directory structure in place
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

---
+++ src/ArrheniusModel.jl
@@ -0,0 +1,5 @@
+module ArrheniusModel
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ArrheniusModel
+using Test
+
+@testset "ArrheniusModel.jl" begin
+    # Write your tests here.
+end
