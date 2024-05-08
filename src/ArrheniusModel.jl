f172e591f23e0a2ddf3d6adbfbd8df1f8ad229aa
Rachel Kurchin <rkurchin@cmu.edu>
2024-05-08 13:51:10 -0400

    remove Manifest from tracking and add to gitignore, add StaticArrays dep, set up some basic tests and check that they run
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- src/ArrheniusModel.jl
+++ src/ArrheniusModel.jl
@@ -1,5 +1,11 @@
 module ArrheniusModel
 
-# Write your package code here.
+include("EnergyStruct.jl")
+include("simulation.jl")
+# TODO: add the rest of them
+
+export PhaseEnergies, n_phases
+export simulate_deposition, most_preferable_state
+# TODO: add exports for anything else you would want to use extnerally, i.e. not functions that are just internal "helper" things
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,13 @@ using Test
 
 @testset "ArrheniusModel.jl" begin
     # Write your tests here.
+    # divide tests up into multiple subsets, @testset's can be nested
+    # here's an example, feel free to add to/change it
+    @testset "PhaseEnergies struct" begin
+        G = [0.0,0.0]
+        Ea = [0.1 0.2; 0.3 0.4]
+        pe = PhaseEnergies(G, Ea)
+        @test n_phases(pe)==2
+        @test_throws AssertionError PhaseEnergies([0,0,0],Ea)
+    end
 end
