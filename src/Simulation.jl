ac7b7429ac747335a58ab74ee13645c72ca5badf
heyyeahcrow <heyyeahcrow.mse05@g2.nctu.edu.tw>
2024-06-04 02:04:39 -0400

    "Apply DEs, no longer conserved, weird..."

--- src/Simulation.jl
+++ src/Simulation.jl
@@ -7,7 +7,39 @@ To build the meshgrids for the x and y values (Temperature and Flow Rate)
     return x_grid, y_grid
 end
 
+function deposition_rates!(dc, c, p, t)
+    # Unpack parameters
+    fcoeff, pe, T, j0, j, dt, num_steps = p
+
+    # Calculate rate constants for all phases
+    arrhenius_rate(pe, T)
+
+    # Calculate deposition rates
+    j = floor(Int, t / dt) + 1
+    f = reverse(fcoeff[j: num_steps+j-1])
+    dc .= c .* f * pe.K
+    if j != j0
+        c[j, 1] = 1.0
+        j = j0
+    end
+end
+
 function simulate_deposition(fcoeff, pe::PhaseEnergies, T, num_steps, dt)
+    # Initialize existing_layers as a 2D array
+    n = n_phases(pe)
+    c0 = zeros(num_steps, n)
+
+    j = 0
+    j0 = 0
+    p = (fcoeff, pe, T, j0, j, dt, num_steps)
+    tspan = (0.0, (num_steps-1) * dt)
+    prob = ODEProblem(deposition_rates!, c0, tspan, p)
+    sol = solve(prob, Euler(), saveat = 0.5, dt=0.1)
+
+    return sol.u[end]
+end
+
+function oldsimulate_deposition(fcoeff, pe::PhaseEnergies, T, num_steps, dt)
 """
 Inputs:
     fcoeff: flow coefficients for each phase
@@ -39,6 +71,8 @@ Output:
     end
     return existing_layers
 end
+
+
 
 function most_preferable_state(composition, threshold, phase_names)
     # Average all the layers
