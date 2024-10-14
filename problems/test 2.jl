79c4f5e35bdbbc98ed79824fc39dab1b94bc3e89
heyyeahcrow <62277691+heyyeahcrow@users.noreply.github.com>
2024-10-14 14:22:24 -0400

    Remove the dot and it works(?)
     Need to check if that dot matters

--- problems/test 2.jl
+++ problems/test 2.jl
@@ -8,6 +8,7 @@ flow_rate = 0.5
 barriers = [0.0 0.2 0.4; 0.2 0.0 0.3; 0.4 0.3 0.0]
 #barriers = [0.0 0.2; 0.2 0.0]
 K = arrhenius_rate(barriers, T)
+#fcoeff = flow_coefficient.("exponential", num_layers, decay_constant*flow_rate)
 n = size(barriers, 1)
 t = 60 # seconds
 dt = 0.5 # seconds
@@ -16,20 +17,26 @@ num_layers = floor(Int, t/0.5)+1
 j = 0
 j0 = 0
 tspan = (0.0, (num_steps-1) * dt)
-decay_constant = 0.0001
+decay_constant = 0.01
 
 function fiip(dc, c, p, t)
     #T, flow_rate = p
     K = arrhenius_rate(barriers, p[1])
-    fcoeff = flow_coefficient.("exponential", num_layers, decay_constant*p[2])
+    fcoeff = flow_coefficient("exponential", num_layers, decay_constant*p[2])
     j = floor(Int, t / 0.5) + 1
     f = reverse(fcoeff[j: num_layers+j-1])
+    #f = ones(num_layers,1) .* exp.(-(1:num_layers) * p[2])
     """
     When I use dc .= c .*f * K, it gives me activity error
     Constant memory is stored (or returned) to a differentiable variable.
     """
+<<<<<<< HEAD
+    dc .= c * K .* f
+    #dc .= c .*f * K
+=======
     # dc .= c * K .*f
     dc .= c .*f * K
+>>>>>>> 27c41f595e243d6b7154b2a438519410f8d34829
     if j != j0
         c[j+1, 1] = 1.0
         j = j0
@@ -69,4 +76,5 @@ dT = zero(T_)
 #Enzyme.autodiff(Reverse, last!, Duplicated(c0, dc0), Duplicated(T_, dT), Duplicated(f_, df), Const(prob))
 Enzyme.autodiff(Reverse, last!, Duplicated(c0, dc0), Duplicated(p,dp), Const(prob))
 
-#df somehow returns 0.0
+
+
